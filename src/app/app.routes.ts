5648bcf393f29bb09a74fd9f04764fb8b53fbb65
Cesar Cruz <cesarcruz.ti@gmail.com>
2025-08-30 14:57:51 -0300

    Add Angular Material and CDK dependencies, implement theming in styles, and create UnauthorisedComponent for handling access control. Update HomeComponent to display user information and enhance layout with a new header.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,17 @@ import { Routes } from '@angular/router';
 import { canActivateAuthRole } from './guards/auth.guard';
 import { HomeComponent } from './components/home/home.component';
 import { environment } from '../environments/environment';
+import { UnauthorisedComponent } from './components/unauthorised/unauthorised.component';
 
 
 export const routes: Routes = [
     {
+        path: 'unauthorised',
+        component: UnauthorisedComponent
+    },
+    {
         path: '',
-        pathMatch: 'full',
+        pathMatch: 'full',  
         component: HomeComponent,
         data: {
             role: [environment.keycloak.role]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,28 @@
 import { Component, inject } from '@angular/core';
 import Keycloak from 'keycloak-js';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   private readonly keycloak = inject(Keycloak);
+  userProfile: any;
+
+  async ngOnInit() {
+    try {
+      this.userProfile = await this.keycloak.loadUserProfile();
+    } catch (error) {
+      console.error('Failed to load user profile', error);
+    }
+  }
+
   logout() {
     this.keycloak.logout();
   }
+  
 }
