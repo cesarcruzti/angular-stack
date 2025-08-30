f641ff3d36e82355027f14b4ec3ba0ed7045b701
Cesar Cruz <cesarcruz.ti@gmail.com>
2025-08-30 15:39:12 -0300

    Refactor HomeComponent layout to use Angular Material's sidenav and toolbar, enhancing user experience with a structured sidebar and main content area. Update styles for new layout components and improve responsiveness.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,22 @@
 import { Component, inject } from '@angular/core';
 import Keycloak from 'keycloak-js';
-import {MatIconModule} from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
 
 
 @Component({
   selector: 'app-home',
-  imports: [MatIconModule],
+  imports: [MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule,
+    MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
