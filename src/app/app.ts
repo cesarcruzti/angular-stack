d7e77d660558f55954d1fea29dd421e4fe5c055f
Cesar Cruz <cesarcruz.ti@gmail.com>
2025-09-02 19:02:04 -0300

    Refactor app layout to implement a responsive sidenav and toolbar using Angular Material. Update HomeComponent to remove old layout and integrate user authentication features, including login/logout functionality and user profile display. Enhance styles for improved UI consistency.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,49 @@
-import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
+import Keycloak from 'keycloak-js';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+    RouterOutlet,
+    CommonModule,
+    RouterLink, 
+    RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('angular-app');
+  readonly keycloak = inject(Keycloak);
+  userProfile: any;
+
+  async ngOnInit() {
+    try {
+      this.userProfile = await this.keycloak.loadUserProfile();
+    } catch (error) {
+      console.error('Failed to load user profile', error);
+    }
+  }
+
+  logout() {
+    this.keycloak.logout({redirectUri: window.location.origin + '/'});
+  }
+
+  login() {
+    this.keycloak.login();
+  }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,45 +1,16 @@
 import { Component, inject } from '@angular/core';
-import Keycloak from 'keycloak-js';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { RouterLink, RouterLinkActive } from '@angular/router';
 
 
 
 @Component({
   selector: 'app-home',
-  imports: [MatSidenavModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatListModule,
-    MatButtonModule,
-    MatCardModule,
-    RouterLink, RouterLinkActive],
+  imports: [
+    MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  readonly keycloak = inject(Keycloak);
-  userProfile: any;
-
-  async ngOnInit() {
-    try {
-      this.userProfile = await this.keycloak.loadUserProfile();
-    } catch (error) {
-      console.error('Failed to load user profile', error);
-    }
-  }
-
-  logout() {
-    this.keycloak.logout();
-  }
-
-  login() {
-    this.keycloak.login();
-  }
+  
   
 }
